31913940f96f365ca69cb5c53c42648095da1788
JiapingcHuwu <1009559252@qq.com>
2022-04-03 16:46:32 +0800

    first update

---
+++ name_change.py
@@ -0,0 +1,6 @@
+name="Hello"
+print(name.title()) #自动大写首字母
+
+print(name.lower()) #将所有大写字母变成小写字母
+
+print(name.upper()) #将所有小写字母变成大写字母
---
+++ name_merged.py
@@ -0,0 +1,5 @@
+name1="I"
+name2="love"
+name3="you"
+name=name1 + " " + name2 + " " + name3
+print(name)
